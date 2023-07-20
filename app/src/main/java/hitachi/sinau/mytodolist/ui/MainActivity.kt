5d6e0de43e8921f266041326045c3f363f04745d
Abika Chairul Yusri <acyusri@hitachi-ch.co.id>
2023-07-20 16:07:05 +0700

    [FEAT]: part 3 navigation component

--- app/src/main/java/hitachi/sinau/mytodolist/ui/MainActivity.kt
+++ app/src/main/java/hitachi/sinau/mytodolist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package hitachi.sinau.mytodolist
+package hitachi.sinau.mytodolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hitachi.sinau.mytodolist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/hitachi/sinau/mytodolist/ui/fragment/TaskFragment.kt
@@ -0,0 +1,7 @@
+package hitachi.sinau.mytodolist.ui.fragment
+
+import androidx.fragment.app.Fragment
+import hitachi.sinau.mytodolist.R
+
+class TaskFragment : Fragment(R.layout.fragment_tasks) {
+}
